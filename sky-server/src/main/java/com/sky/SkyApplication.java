c21af23fc121f4651b046ea9dae14c36a80c782a
如烟 <2513645274@qq.com>
2023-05-18 18:36:27 +0800

    服务开启了

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -12,5 +12,6 @@ public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
+        log.info("服务开启了");
     }
 }
